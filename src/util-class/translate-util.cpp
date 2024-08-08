ae11a3d917df99f58d88301d1ee4a10701cbb311
Sujit K M <kmsujit@gmail.com>
2024-08-08 10:25:59 +0530

    made code cleaner for results
    made code cleaner for results

--- src/util-class/translate-util.cpp
+++ src/util-class/translate-util.cpp
@@ -9,8 +9,7 @@ std::string translate(std::string input, std::string src_lang, std::string dest_
 
 std::string translate(std::string base_url, std::string input, std::string src_lang, std::string dest_lang){
     LibreTranslateAPI *libreTranslateAPI = new LibreTranslateAPI(base_url);
-    std::string result ;
-    result = libreTranslateAPI->translate(input, src_lang, dest_lang);
+    std::string result = libreTranslateAPI->translate(input, src_lang, dest_lang) ;
     delete libreTranslateAPI;
     return result;
 }
